bd06e75997dad165c89df56067264db71bb1acc1
j4nbob <j4nbob@gmail.com>
2025-12-01 16:32:30 -0800

    Initialize main.rs for Advent of Code 2025

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+mod day01;
+mod day02;
+mod day03;
+mod day04;
+mod day05;
+mod day06;
+mod day07;
+mod day08;
+mod day09;
+mod day10;
+mod day11;
+mod day12;
+
+fn main() {
+    println!("Advent of Code 2025");
+    println!("===================\n");
+    
+    day01::solve();
+    day02::solve();
+    day03::solve();
+    day04::solve();
+    day05::solve();
+    day06::solve();
+    day07::solve();
+    day08::solve();
+    day09::solve();
+    day10::solve();
+    day11::solve();
+    day12::solve();
+}
